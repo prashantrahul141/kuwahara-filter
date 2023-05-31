36d04f37dd8f38e173d844ab941cfa07e372e419
Prashant <prashantrahul141@protonmail.com>
2023-05-31 09:22:17 +0530

    add command line argument and opening image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use clap::Parser;
+use image::{DynamicImage, GenericImageView};
+use std::process::exit;
+
+// Simple program.
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct Args {
+    /// input filename.
+    #[arg(short = 'f', long,  value_name = "FILENAME", value_hint = clap::ValueHint::FilePath)]
+    filename: String,
+
+    /// kernel size for sampling.
+    #[arg(short = 'k', long, value_name = "KERNEL")]
+    kernal: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // reading command line arguments
+    let _args = Args::parse();
+
+    // check kernal value.
+    if _args.kernal < 3 || _args.kernal % 2 == 0 {
+        println!("[Error] Kernal cannot be smaller than 3, and cannot be divisble by 2.");
+        exit(1)
+    }
+
+    // opening image
+    println!("Reading Image : {}", _args.filename);
+    let image: DynamicImage = match image::open(Args::parse().filename) {
+        Ok(result) => result,
+        Err(err) => {
+            println!("[ERROR] {}", err);
+            exit(1);
+        }
+    };
+
+    // looping through pixels.
+    for i in image.pixels() {
+        print!("{:?}", i);
+    }
 }
