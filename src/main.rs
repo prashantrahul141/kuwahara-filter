730f765bcaffb7f10a16b3b43efd0b1c5b046e1a
Prashant <prashantrahul141@protonmail.com>
2023-05-31 11:32:13 +0530

    calculate quadrants around each pixel

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use image::{DynamicImage, GenericImageView};
+use image::{DynamicImage, GenericImageView, ImageBuffer, RgbImage, Rgba};
 use std::process::exit;
 
 // Simple program.
@@ -12,22 +12,22 @@ struct Args {
 
     /// kernel size for sampling.
     #[arg(short = 'k', long, value_name = "KERNEL")]
-    kernal: u32,
+    kernal: i32,
 }
 
 fn main() {
     // reading command line arguments
-    let _args = Args::parse();
+    let args = Args::parse();
 
     // check kernal value.
-    if _args.kernal < 3 || _args.kernal % 2 == 0 {
+    if args.kernal < 3 || args.kernal % 2 == 0 {
         println!("[Error] Kernal cannot be smaller than 3, and cannot be divisble by 2.");
         exit(1)
     }
-
+    let qdrnt_size = (args.kernal - 1) / 2;
     // opening image
-    println!("Reading Image : {}", _args.filename);
-    let image: DynamicImage = match image::open(Args::parse().filename) {
+    println!("Reading Image : {}", args.filename);
+    let original_image: DynamicImage = match image::open(Args::parse().filename) {
         Ok(result) => result,
         Err(err) => {
             println!("[ERROR] {}", err);
@@ -35,8 +35,48 @@ fn main() {
         }
     };
 
+    let new_image: RgbImage = ImageBuffer::new(original_image.width(), original_image.height());
+
     // looping through pixels.
-    for i in image.pixels() {
-        print!("{:?}", i);
+    for y in 0i32..original_image.height() as i32 {
+        for x in 0i32..original_image.width() as i32 {
+            // current pixel on the original image.
+            // let current_pixel = original_image.get_pixel(x as u32, y as u32);
+
+            // quadrants around pixels.
+            let mut quadrants: Vec<Vec<Rgba<u8>>> = vec![vec![], vec![], vec![], vec![]];
+
+            // looping through sectors for each pixel.
+            for operation_y in (y - qdrnt_size)..(y + qdrnt_size) {
+                if operation_y < 0
+                    || operation_y >= original_image.height() as i32
+                    || x == operation_y
+                {
+                    continue;
+                }
+
+                for operation_x in (x - qdrnt_size)..(x + qdrnt_size) {
+                    if operation_x < 0
+                        || operation_x >= original_image.width() as i32
+                        || x == operation_x
+                    {
+                        continue;
+                    }
+
+                    // checking which quadrant pixel belongs to adding its color to it.
+                    let qudrant_index = match (operation_x > x, operation_y > y) {
+                        (true, true) => 3,
+                        (false, true) => 2,
+                        (true, false) => 1,
+                        (false, false) => 0,
+                    };
+
+                    quadrants[qudrant_index]
+                        .push(original_image.get_pixel(operation_x as u32, operation_y as u32));
+                }
+            }
+
+            // calculate average of all quadrants.
+        }
     }
 }
